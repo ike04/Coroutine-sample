7090b91c32154867e323bcc3cf0d2c9a36bf1545
Issei Ikeda <issei.ikeda@zozo.com>
2022-06-03 15:07:18 +0900

    RecyclerViewを実装

---
+++ app/src/main/java/com/example/coroutine_sample/ArticleFactory.kt
@@ -0,0 +1,25 @@
+package com.example.coroutine_sample
+
+import android.view.View
+import com.example.coroutine_sample.databinding.ViewListCellBinding
+import com.xwray.groupie.viewbinding.BindableItem
+
+class ArticleFactory(private val article: Article) :
+    BindableItem<ViewListCellBinding>() {
+    override fun getLayout() = R.layout.view_list_cell
+
+    override fun bind(viewBinding: ViewListCellBinding, position: Int) {
+        viewBinding.item = article
+    }
+
+    override fun initializeViewBinding(view: View): ViewListCellBinding {
+        return ViewListCellBinding.bind(view)
+    }
+
+//    override fun isSameAs(other: Item<*>): Boolean = (other as? ArticleFactory)?.article?.id == article.id
+}
+
+data class Article(
+    val title: String,
+    val author: String
+)
--- app/src/main/java/com/example/coroutine_sample/ListFragment.kt
+++ app/src/main/java/com/example/coroutine_sample/ListFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.coroutine_sample.databinding.ListFragmentBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 
 class ListFragment : Fragment() {
 
@@ -16,13 +18,26 @@ class ListFragment : Fragment() {
 
     private lateinit var binding: ListFragmentBinding
     private val viewModel: ListViewModel by viewModels()
+    private val groupAdapter = GroupAdapter<GroupieViewHolder>()
+    private val testData = mutableListOf<Article>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = ListFragmentBinding.inflate(layoutInflater)
+        binding = ListFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.adapter = groupAdapter
+
+        repeat(10) {
+            testData.add(Article("テスト$it", "作者$it"))
+        }
+
+        groupAdapter.update(testData.map { ArticleFactory(it) })
     }
 }
