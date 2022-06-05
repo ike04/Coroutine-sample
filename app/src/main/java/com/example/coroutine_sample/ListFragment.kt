ad21c03c8bf8d58efeeacc1c7fb0e0916b3f2699
Issei Ikeda <issei.ikeda@zozo.com>
2022-06-05 13:45:39 +0900

    エラーの処理を実装

--- app/src/main/java/com/example/coroutine_sample/ListFragment.kt
+++ app/src/main/java/com/example/coroutine_sample/ListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.coroutine_sample.databinding.ListFragmentBinding
@@ -33,8 +34,12 @@ class ListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.recyclerView.adapter = groupAdapter
 
-        viewModel.articleList.observe(viewLifecycleOwner) { article ->
+        viewModel.articleList?.observe(viewLifecycleOwner) { article ->
             article?.map { ArticleFactory(it) }?.let { groupAdapter.update(it) }
+        }
+
+        viewModel.error?.observe(viewLifecycleOwner) { error ->
+            Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
         }
 
         viewModel.fetchArticle("Kotlin", 1)
--- app/src/main/java/com/example/coroutine_sample/ListViewModel.kt
+++ app/src/main/java/com/example/coroutine_sample/ListViewModel.kt
@@ -6,11 +6,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 class ListViewModel : ViewModel() {
     private val repository = SearchRepositoryImpl()
     private val _articleList = MutableLiveData<List<Article>?>()
-    val articleList = _articleList as LiveData<List<Article>?>
+    private val _error = MutableLiveData<Failure>()
+    val articleList = _articleList as? LiveData<List<Article>?>
+    val error = _error as? LiveData<Failure>
 
     fun fetchArticle(tag: String, page: Int) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -18,7 +22,21 @@ class ListViewModel : ViewModel() {
                 val articleList = repository.fetchArticles(tag, page)
                 _articleList.postValue(articleList)
             } catch (e: Exception) {
+                _error.postValue(Failure(e, e.toMessage()))
             }
         }
+    }
+}
+
+data class Failure(
+    val error: Throwable,
+    val message: String
+)
+
+fun Throwable.toMessage(): String {
+    return when (this) {
+        is HttpException -> "エラーが発生しました。"
+        is UnknownHostException -> "ネットワークに接続できません。"
+        else -> "エラーが発生しました。"
     }
 }
